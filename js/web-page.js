547d9f07fb1da579496327bf2b544dbaa6f1e5bb
Stuart Lees <stuartlees@gmail.com>
2014-05-13 19:19:46 -0500

    Initial setup and structure
    - Created base WebPage Controller module
    - Applying very small CSS reset
    - Added iframe
    - Google Analytics code
    - Disabling context menu
    - Initializing WebPage Controller

--- js/web-page.js
+++ js/web-page.js
@@ -1,3 +1,20 @@
-/**
- * Created by Stuart on 2014-05-06.
- */
+var RiseVision = RiseVision || {};
+RiseVision.WebPage = {};
+
+RiseVision.WebPage.Controller = (function($,gadgets) {
+  "use strict";
+
+  // private variables
+  var _prefs = null;
+
+  // private functions
+
+  // public space
+  return {
+    init: function(){
+      _prefs = new gadgets.Prefs();
+    }
+  }
+
+})($,gadgets);
+
