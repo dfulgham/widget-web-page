bccc68b7251fb76aa0e7e0b9dfe9d152379ab595
Stuart Lees <stuartlees@gmail.com>
2014-05-23 15:08:49 -0500

    Fix for QA bug #3
    - Disabling Scrollbar checkbox if zoom value is more than 100%
    - Changed validation function names to be in line with new names from common.js
    - Copy correction on label for Data Refresh

--- js/settings.js
+++ js/settings.js
@@ -5,7 +5,9 @@ RiseVision.WebPage.Settings = (function($,gadgets, i18n) {
   "use strict";
 
   // private variables
-  var _prefs = null;
+  var _prefs = null,
+      _currentZoom = 1,
+      _el;
 
   // private functions
   function _bind(){
@@ -23,42 +25,78 @@ RiseVision.WebPage.Settings = (function($,gadgets, i18n) {
         "free-gadgets/rise-vision-url/", "_blank");
     });
 
-    $("#interactive").on("click", function(event) {
-      if ($(this).is(":checked")) {
-        $("#interactivity > div + div.checkbox").show();
-      } else {
-        if ($("#scrollbars").is(":checked")) {
-          $("#scrollbars").click();
+    _el.zoomSel.on('change', function() {
+      //alert( this.value ); // or $(this).val()
+      if($(this).val() > 1){
+        if (!_el.scrollbarsCB.is(':disabled')){
+          if(_el.scrollbarsCB.is(":checked")) {
+            _el.scrollbarsCB.click();
+          }
+          _el.scrollbarsCB.prop('disabled', true);
+          $("label[for='scrollbars']").addClass('label-disabled').css(
+            'cursor','default');
         }
-        $("#interactivity > div + div.checkbox").hide();
+        _currentZoom = $(this).val();
+      } else {
+        if (_el.scrollbarsCB.is(':disabled')){
+          _el.scrollbarsCB.prop('disabled', false);
+          $("label[for='scrollbars']").removeClass('label-disabled').css(
+            'cursor','pointer');
+        }
       }
     });
+
+    _el.interactiveCB.on("click", function(event) {
+      if ($(this).is(":checked")) {
+        _el.scrollbarsCtn.show();
+      } else {
+        if(_currentZoom <= 1 && _el.scrollbarsCB.is(":checked")){
+          _el.scrollbarsCB.click();
+        }
+        _el.scrollbarsCtn.hide();
+      }
+    });
+  }
+
+  function _cache(){
+    _el = {
+      wrapperCtn:           $(".widget-wrapper"),
+      urlInp:               $("#url"),
+      alertCtn:             $("#settings-alert"),
+      scrollHorizInp:       $("#scroll-horizontal"),
+      scrollVertInp:        $("#scroll-vertical"),
+      interactiveCB:        $("#interactive"),
+      scrollbarsCtn:        $("#interactivity > div + div.checkbox"),
+      scrollbarsCB:         $("#scrollbars"),
+      zoomSel:              $("#zoom"),
+      dataRefreshSel:       $("#refresh")
+    }
   }
 
   function _getValidationsMap(){
     return {
       "required": {
-        fn: RiseVision.Common.Validation.required,
+        fn: RiseVision.Common.Validation.isValidRequired,
         localize: "validation.required",
         conditional: null
       },
       "url": {
-        fn: RiseVision.Common.Validation.url,
+        fn: RiseVision.Common.Validation.isValidURL,
         localize: "validation.valid_url",
         conditional: null
       },
       "numeric": {
-        fn: RiseVision.Common.Validation.numeric,
+        fn: RiseVision.Common.Validation.isValidNumber,
         localize: "validation.numeric",
         conditional: null
       },
       "horizontal_scroll": {
-        fn: RiseVision.Common.Validation.lessThan,
+        fn: RiseVision.Common.Validation.isLessThan,
         localize: "validation.scroll_size",
         conditional: _prefs.getString("rsW")
       },
       "vertical_scroll": {
-        fn: RiseVision.Common.Validation.lessThan,
+        fn: RiseVision.Common.Validation.isLessThan,
         localize: "validation.scroll_size",
         conditional: _prefs.getString("rsH")
       }
@@ -68,19 +106,19 @@ RiseVision.WebPage.Settings = (function($,gadgets, i18n) {
   function _getAdditionalParams(){
     var additionalParams = {};
 
-    additionalParams["url"] = $("#url").val();
+    additionalParams["url"] = _el.urlInp.val();
 
     return additionalParams;
   }
 
   function _getParams(){
     var params = "&up_scroll-horizontal=" +
-      $.trim($("#scroll-horizontal").val()) + "&up_scroll-vertical=" +
-      $.trim($("#scroll-vertical").val()) + "&up_zoom=" +
-      $("#zoom").val() + "&up_interactive=" +
-      $("#interactive").is(":checked").toString() + "&up_scrollbars=" +
-      $("#scrollbars").is(":checked").toString() + "&up_data-refresh=" +
-      $("#refresh").val();
+      $.trim(_el.scrollHorizInp.val()) + "&up_scroll-vertical=" +
+      $.trim(_el.scrollVertInp.val()) + "&up_zoom=" +
+      _el.zoomSel.val() + "&up_interactive=" +
+      _el.interactiveCB.is(":checked").toString() + "&up_scrollbars=" +
+      _el.scrollbarsCB.is(":checked").toString() + "&up_data-refresh=" +
+      _el.dataRefreshSel.val();
 
     return params;
   }
@@ -90,8 +128,8 @@ RiseVision.WebPage.Settings = (function($,gadgets, i18n) {
 
     // validate
     if(!_validate()){
-      $("#settings-alert").show();
-      $(".widget-wrapper").scrollTop(0);
+      _el.alertCtn.show();
+      _el.wrapperCtn.scrollTop(0);
     } else {
       //construct settings object
       settings = {
@@ -121,7 +159,7 @@ RiseVision.WebPage.Settings = (function($,gadgets, i18n) {
         ],
         passed = true;
 
-    $("#settings-alert").empty().hide();
+    _el.alertCtn.empty().hide();
 
     for(var i = 0; i < itemsToValidate.length; i++){
       if(!_validateItem(itemsToValidate[i])){
@@ -157,9 +195,10 @@ RiseVision.WebPage.Settings = (function($,gadgets, i18n) {
   return {
     init: function(){
 
+      _cache();
       _bind();
 
-      $("#settings-alert").hide();
+      _el.alertCtn.hide();
 
       //Request additional parameters from the Viewer.
       gadgets.rpc.call("", "rscmd_getAdditionalParams", function(result) {
@@ -169,28 +208,33 @@ RiseVision.WebPage.Settings = (function($,gadgets, i18n) {
         if (result) {
           result = JSON.parse(result);
 
-          $("#scroll-horizontal").val(_prefs.getString("scroll-horizontal"));
-          $("#scroll-vertical").val(_prefs.getString("scroll-vertical"));
-          $("#zoom").val(_prefs.getString("zoom"));
-          $("#interactive").attr("checked", _prefs.getBool("interactive"));
-          $("#scrollbars").attr("checked", _prefs.getBool("scrollbars"));
-          $("#refresh").val(_prefs.getString("data-refresh"));
+          _el.scrollHorizInp.val(_prefs.getString("scroll-horizontal"));
+          _el.scrollVertInp.val(_prefs.getString("scroll-vertical"));
+          _el.zoomSel.val(_prefs.getString("zoom"));
+          _el.interactiveCB.attr("checked", _prefs.getBool("interactive"));
+          _el.scrollbarsCB.attr("checked", _prefs.getBool("scrollbars"));
+          if(_prefs.getString("zoom") > 1){
+            _el.scrollbarsCB.prop('disabled', true);
+            $("label[for='scrollbars']").addClass('label-disabled').css(
+              'cursor','default');
+          }
+          _el.dataRefreshSel.val(_prefs.getString("data-refresh"));
 
           //Additional params
-          $("#url").val(result["url"]);
+          _el.urlInp.val(result["url"]);
 
         } else {
           // initialize input elements with defaults
-          $("#scroll-horizontal").val(0);
-          $("#scroll-vertical").val(0);
+          _el.scrollHorizInp.val(0);
+          _el.scrollVertInp.val(0);
         }
 
         /* Manually trigger event handlers so that the visibility of fields
            can be set. */
-        $("#interactive").triggerHandler("click");
+        _el.interactiveCB.triggerHandler("click");
 
         i18n.init({ fallbackLng: "en" }, function(t) {
-          $(".widget-wrapper").i18n().show();
+          _el.wrapperCtn.i18n().show();
           $(".form-control").selectpicker();
 
           // Set tooltips only after i18n has shown
@@ -200,9 +244,9 @@ RiseVision.WebPage.Settings = (function($,gadgets, i18n) {
 
           //Set buttons to be sticky only after wrapper is visible.
           $(".sticky-buttons").sticky({
-              container : $(".widget-wrapper"),
+              container : _el.wrapperCtn,
               topSpacing : 41,	//top margin + border of wrapper
-              getWidthFrom : $(".widget-wrapper")
+              getWidthFrom : _el.wrapperCtn
           });
 
         });
